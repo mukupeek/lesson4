78a656b983f5e3b01e4dffc2298cd479945bde65
Denis <fd.fd.com@yandex.ru>
2021-02-01 09:06:00 +0300

    2.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,61 +3,35 @@ import java.util.concurrent.Executors;
 
 public class Main {
     private static Object key = new Object();
-    private static volatile char curChar = 'A';
-    private static volatile char nextChar = 'B';
+    private static volatile char curChar;
+    private static volatile char nextChar;
+    private static volatile char printChar = 'A';
+    private static ExecutorService executorService;
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        executorService.submit(() -> {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    synchronized (key) {
-                        while (curChar != 'A') {
-                            key.wait();
-                        }
-                        System.out.println("A");
-                        curChar = 'B';
-                        key.notifyAll();
-                    }
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-        executorService.submit(() -> {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    synchronized (key) {
-                        while (curChar != 'B') {
-                            key.wait();
-                        }
-                        System.out.println("B");
-                        curChar = 'C';
-                        key.notifyAll();
-                    }
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-        executorService.submit(() -> {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    synchronized (key) {
-                        while (curChar != 'C') {
-                            key.wait();
-                        }
-                        System.out.println("C");
-                        curChar = 'A';
-                        key.notifyAll();
-                    }
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
+        executorService = Executors.newFixedThreadPool(3);
+        createThread('A', 'B');
+        createThread('B', 'C');
+        createThread('C', 'A');
+    }
 
-
+    public static void createThread(char curChar, char nextChar) {
+        executorService.submit(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (key) {
+                        while (printChar != curChar) {
+                            key.wait();
+                        }
+                        System.out.println(curChar);
+                        printChar = nextChar;
+                        key.notifyAll();
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 
 }
